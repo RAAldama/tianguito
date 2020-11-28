a54f8fc606fa6b1cbe5e0d139a0e5b9d291f735d
Ricardo Aldama <ricardoaldama98@gmail.com>
2020-11-28 16:41:52 -0600

    So it begins

---
+++ client/src/app/pages/register/register.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  submitUser(user, market){
+    console.log(user, market);
+
+    this.router.navigateByUrl('/tabs/tab1');
+  }
+
+}
--- client/src/app/pages/shop-pages/stand/stand.page.ts
+++ client/src/app/pages/shop-pages/stand/stand.page.ts
@@ -10,27 +10,36 @@ export class StandPage implements OnInit {
   products: any[] = [
     {
       name: "Kilo de Manzanas Rojas",
+      stand: 'Puesto de Fruta de Don Miguel',
       description: "Clásicas manzanas rojas, frescas y recién traídas de provincia.",
       price:  22.50, 
       id: 1
     },
     {
       name: "Kilo de Mango Manila",
+      stand: 'Puesto de Fruta de Don Miguel',
       description: "Nada como un buen mango para el desayuno, los últimos antes del fin de temporada.",
       price:  25.25, 
       id: 2 
     },
     {
       name: "Kilo de Plátano Dominico",
+      stand: 'Puesto de Fruta de Don Miguel',
       description: "Este plátano más pequeño es perfecto para cocteles y se conserva mejor.",
       price:  30.00, 
       id: 3
     }
   ]
 
+  productPage: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  productPageAppear(){
+    this.productPage = !this.productPage;
   }
 
 }
