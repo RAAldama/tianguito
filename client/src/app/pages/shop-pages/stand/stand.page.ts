b23bf5b90acf0a62d396eeff2a2119b285c79143
Ricardo Aldama <ricardoaldama98@gmail.com>
2020-11-29 23:26:01 -0600

    carrito ptm

--- client/src/app/pages/shop-pages/stand/stand.page.ts
+++ client/src/app/pages/shop-pages/stand/stand.page.ts
@@ -13,6 +13,7 @@ export class StandPage implements OnInit {
       stand: 'Puesto de Fruta de Don Miguel',
       description: "Clásicas manzanas rojas, frescas y recién traídas de provincia.",
       price:  22.50, 
+      quantity: 0,
       id: 1
     },
     {
@@ -20,6 +21,7 @@ export class StandPage implements OnInit {
       stand: 'Puesto de Fruta de Don Miguel',
       description: "Nada como un buen mango para el desayuno, los últimos antes del fin de temporada.",
       price:  25.25, 
+      quantity: 0,
       id: 2 
     },
     {
@@ -27,34 +29,55 @@ export class StandPage implements OnInit {
       stand: 'Puesto de Fruta de Don Miguel',
       description: "Este plátano más pequeño es perfecto para cocteles y se conserva mejor.",
       price:  30.00, 
+      quantity: 0,
       id: 3
     }
   ]
 
-  price = this.products[0].price;
+  purchase = [];
+  index: number;
+  price = 0;
+  totalPrice = 0;
 
   productPage: boolean = false;
-
   cartPage: boolean = false;
-
   standPage: boolean = true;
 
   constructor() { 
+    this.index = 0;
   }
 
   ngOnInit() {
   }
 
-  productPageAppear(){
+  productPageAppear(index){
     this.productPage = !this.productPage;
+    this.index = index;
+    this.price = this.products[this.index].price;
   }
 
   productQuantity(value){
     if(  value === 0 || value === undefined ){
-      this.price = this.products[0].price;
+      this.price = this.products[this.index].price;
     }
 
-    this.price = this.products[0].price * value;
+   this.products[this.index].quantity = value;
+    this.price = this.products[this.index].price * value;
+    
+  }
+
+  addProduct(product){
+    this.purchase.push(product);
+    console.log(this.purchase);
+  }
+
+  cart(){
+    this.cartPage = true; 
+    this.standPage = false;
+
+    this.products.forEach(product => {
+      this.totalPrice = (product.price * product.quantity) + this.totalPrice;
+    });
   }
 
 }
